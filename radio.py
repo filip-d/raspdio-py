f79fd663093d0d519ad802e40bf2ffdedf8254cf
filip <pi@raspberrypi.(none)>
2013-06-17 00:32:24 +0000

    lets make some noise

--- radio.py
+++ radio.py
@@ -3,12 +3,18 @@
 import sys
 import mpd
 import requests
+import pygame
 
 from select import select
 from sets import Set
 from decimal import *
 getcontext().prec = 4
 
+pygame.init()
+pygame.mixer.init()
+
+s = pygame.mixer.Sound('noise.ogg')
+noise = s.play(loops=-1)
 
 def print_at(x, y, text):
      sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
@@ -18,13 +24,21 @@ def cls():
     sys.stderr.write("\x1b[2J\x1b[H")
 
 def good_frequency(selected_freq, all_frequencies):
-    #print_at(15,1, "Freq type {0}".format(all_frequencies))
-    if selected_freq in all_frequencies:
-        #print_at (14,1, "Radio found at {0}".format(selected_freq))
-        return True
+    all_frequencies = sorted(all_frequencies)
+    print_at(15,1, "Freq type {0}".format(all_frequencies))
+    prev_freq = 0
+    for f in all_frequencies:
+    	if (f >= selected_freq):
+            if (f-selected_freq < selected_freq-prev_freq):
+	        print_at (14,1, "Radio found at {0}, {1} difference".format(f,f-selected_freq))
+		return [f, f-selected_freq]
+	    else:
+                print_at (14,1, "Radio found at {0}, {1} difference".format(prev_freq,selected_freq-prev_freq))
+                return [prev_freq, selected_freq-prev_freq]
+	prev_freq = f
     
-    #print_at (14,1, "No radio found at {0}".format(selected_freq))
-    return False
+    return [0,1000]
+
 
 def main(argv):
     cls()
@@ -32,6 +46,7 @@ def main(argv):
     timeout = 1500
     freq = Decimal(104)
     freq_changed = True
+    tuned_in = 0
     min_freq = Decimal(88.0)
     max_freq = Decimal(108.0)
     frequencies = Set()
@@ -63,26 +78,41 @@ def main(argv):
 	    if freq_changed:
                 cls()
                 print_at (1, 1, "Frequency: %s " % freq)
-                if good_frequency(freq, frequencies):
-   	            url = "http://radio.7digital.fm/radio/key/%s/nowplaying" % freq
-		    print_at (2,1, url)
-		    raw_r = requests.get("http://radio.7digital.fm/radio/key/%s/nowplaying" % freq)
-		    r = raw_r.json()
-		    print_at (3,1, "Response status: {0}".format(raw_r.status_code))
-		    print_at (4,1, "Now playing: {0}".format(r["artist_name"]))
-		    url = r["preview_url"]
-		    print_at (5,1, "Stream url: {0}".format(url))
-		    pos = r["play_position"]
-		    print_at (6,1, "Play position: {0}".format(pos))
-		    #print client.status()
-		    client.command_list_ok_begin() # start a command list
-		    client.clear() # insert the update command into the list
-		    client.add(url)
-		    client.seek(0,pos)
-		    client.play()
-		    results = client.command_list_end()  # results will be a list with the results			
+		closest_good_freq = good_frequency(freq, frequencies)
+		gfreq = closest_good_freq[0]
+		gfreq_diff = closest_good_freq[1]
+                if gfreq_diff<=1:
+		    if (tuned_in != gfreq):
+                        url = "http://radio.7digital.fm/radio/key/%s/nowplaying" % gfreq
+     	                print_at (22,1, url)
+                    	raw_r = requests.get("http://radio.7digital.fm/radio/key/%s/nowplaying" % gfreq)
+                    	r = raw_r.json()
+                    	print_at (23,1, "Response status: {0}".format(raw_r.status_code))
+                    	print_at (24,1, "Now playing: {0}".format(r["artist_name"]))
+                    	url = r["preview_url"]
+                    	print_at (25,1, "Stream url: {0}".format(url))
+                    	pos = r["play_position"]
+                    	print_at (26,1, "Play position: {0}".format(pos))
+                    	#print client.status()
+ 
+		    	client.command_list_ok_begin() # start a command list
+		    	client.clear() # insert the update command into the list
+		    	client.add(url)
+		    	client.seek(0,pos)
+		    	client.play()
+		    	results = client.command_list_end()  # results will be a list with the results			
+			tuned_in = gfreq
 		    freq_changed = False
-			
+		    noise.set_volume(gfreq_diff)
+		else:
+		    if tuned_in>0:
+			noise.set_volume(0.9)
+			tuned_in = 0
+	    print_at (30,1, "MPD status {0}".format(client.status()["state"]))
+	    if (client.status()["state"]=="stop"):
+		tuned_in = 0
+		freq_changed=True
+		
             rl, wl, xl = select([sys.stdin], [], [], timeout)
             if rl: # some input
                 c = sys.stdin.read(1)
@@ -102,11 +132,15 @@ def main(argv):
             else:
                 # timeout
                 break
-		
+        client.stop()
+        client.close()
+        client.disconnect()		
+
+
     finally:
-	client.stop()
-	client.close()
-	client.disconnect()
+#	client.stop()
+#	client.close()
+#	client.disconnect()
 
 	# restore non-raw input
     	if prev_flags is not None:
